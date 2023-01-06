277d212de73e8daedab723c2b11343b76f5bc6df
Giovanna Andrade Santos <66024466+gioandrade7@users.noreply.github.com>
2023-01-06 19:25:25 -0400

    Update analisadorLexico.c

--- analisadorLexico.c
+++ analisadorLexico.c
@@ -1,20 +1,81 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<string.h>
 
-char prox_char(FILE *f){
-    return fgetc(f);
+int i = 0; // variável global para percorrer a linha
+char prox_char(char *linha){
+    if(linha[i] != '\n'){
+        char c = linha[i];
+        i++;
+        return c;
+    }
+    else{
+        i = 0;
+        return;
+    }
+}
+
+char sinais_pontuacao_token(char c){ //verificador de token para sinais de pontuação
+    int estado = 0;
+    while(1){
+        switch (estado){
+        case 0:
+            if(c == ','){estado = 1;}
+            else if(c == ';'){estado = 2;}
+            else if(c == '('){estado = 3;}
+            else if(c == ')'){estado = 4;}
+            else if(c == '['){estado = 5;}
+            else if(c == ']'){estado = 6;}
+            else if(c == '{'){estado = 7;}
+            else if(c == '}'){estado = 8;}
+            /*else{} o que fazer quando nenhum caminho servir ?*/
+            break;
+        case 1:
+            return ',';
+            break;
+        case 2:
+            return ';';
+            break;
+        case 3:
+            return '(';
+            break;
+        case 4:
+            return ')'
+            break;
+        case 5:
+            return '[';
+            break;
+        case 6:
+            return ']';
+            break;
+        case 7:
+            return '{';
+            break;
+        case 8:
+            return '}';
+            break;
+        }
+        
+    }
 }
 
 int main(int agrc, char *argv[]){
 
-    FILE *teste = fopen(argv[1], "rb");
+    FILE *teste = fopen(argv[1], "rb"); //abertura do arquivo
     if(teste == NULL){
         printf("ERRO!");
     }
-    
-    printf("%c", prox_char(teste));
 
+    char str[200];
+    fgets(str, 200, teste);
+    for(int j = 0; j <11; j++){printf("%c", prox_char(str));}
+    printf("\n");
+
+    fgets(str, 200, teste);
+    for(int j = 0; j <11; j++){printf("%c", prox_char(str));}
+    printf("\n");
+    
     fclose(teste);
     
 }
