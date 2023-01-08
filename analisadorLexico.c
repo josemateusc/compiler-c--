a4135d9cd9d5cdd6a0cc58c0e414c57b2e237d37
giandrade7 <giovannas058@gmail.com>
2023-01-08 18:44:18 -0400

    upload analisadorLexico.c

--- analisadorLexico.c
+++ analisadorLexico.c
@@ -1,10 +1,27 @@
+#define _GNU_SOURCE // necessário porque getline() é extensão GNU
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include<string.h>
+#include<ctype.h>
 
 int i = 0; // variável global para percorrer a linha
-char prox_char(char *linha){
+FILE *docLex;
+char linha[20000] = "";
+ //buffer arbitrário
+
+void grava_token(char *token, char *lexema){
+    char buffer[strlen(token)+strlen(lexema)+2];
+    strcat(buffer,token);
+    strcat(buffer,"\t");
+    strcat(buffer,lexema);
+    strcat(buffer,"\n");
+    fwrite(buffer, sizeof(buffer), 1, docLex);
+}
+
+char prox_char(){
+    if(strlen(linha) == 0){
+        fgets(linha, 20000, file);
+    }
     if(linha[i] != '\n'){
         char c = linha[i];
         i++;
@@ -12,70 +29,172 @@ char prox_char(char *linha){
     }
     else{
         i = 0;
-        return;
+        linha = "";
+        return '\n';
     }
 }
 
-char sinais_pontuacao_token(char c){ //verificador de token para sinais de pontuação
+void avanca(int* j, char* lexema, char* c){
+    lexema[j] = c;
+    j++;
+    c = prox_char();
+}
+
+int estado0(char c){ //função que simula estado 0
+    int estado;
+    if(isalpha(c)){estado = 1;}
+    else if(isdigit(c)){estado = 3;}
+    else if(c == '"'){estado = 8;}
+    else if(c == '\''){estado = 10;}
+    else if(c == '/'){estado = 13;}
+    else if(c == '+'){estado = 20;}
+    else if(c == '-'){estado = 24;}
+    else if(c == '*'){estado = 29;}
+    else if(c == '%'){estado = 32;}
+    else if(c == '='){estado = 35;}
+    else if(c == '!'){estado = 38;}
+    else if(c == '>'){estado = 41;}
+    else if(c == '<'){estado = 44;}
+    else if(c == '<'){estado = 44;}
+    else if(c == '|'){estado = 47;}
+    else if(c == '&'){estado = 49;}
+    else if(c == ','){estado = 52;}
+    else if(c == ';'){estado = 53;}
+    else if(c == '.'){estado = 54;}
+    else if(c == '('){estado = 55;}
+    else if(c == ')'){estado = 56;}
+    else if(c == ']'){estado = 57;}
+    else if(c == '['){estado = 58;}
+    else if(c == '{'){estado = 59;}
+    else if(c == '}'){estado = 60;}
+    else if(c == ':'){estado = 61;}
+    else if(c == '?'){estado = 62;}
+    else if(c == ' ' || c == '\t' || c == '\n'){;}
+    else{erro();} //"simbolo inválido"
+    return estado
+}
+
+char* analex(){ //classificador de token
+    char c = prox_char();  
     int estado = 0;
+    char token[30]; //,valor arb
+    char lexema[100];
+    int j = 0;
     while(1){
         switch (estado){
-        case 0:
-            if(c == ','){estado = 1;}
-            else if(c == ';'){estado = 2;}
-            else if(c == '('){estado = 3;}
-            else if(c == ')'){estado = 4;}
-            else if(c == '['){estado = 5;}
-            else if(c == ']'){estado = 6;}
-            else if(c == '{'){estado = 7;}
-            else if(c == '}'){estado = 8;}
-            /*else{} o que fazer quando nenhum caminho servir ?*/
-            break;
-        case 1:
-            return ',';
-            break;
-        case 2:
-            return ';';
-            break;
-        case 3:
-            return '(';
-            break;
-        case 4:
-            return ')'
-            break;
-        case 5:
-            return '[';
-            break;
-        case 6:
-            return ']';
-            break;
-        case 7:
-            return '{';
-            break;
-        case 8:
-            return '}';
-            break;
+            case 0:
+                estado = estado0(c);
+                avanca(&j, lexema, &c);
+                break;
+            case 1:
+                if(isdigit(c) || isalpha(c)){;}
+                else if(!(isdigit(c) || isalpha(c))){estado = 2;}
+                avanca(&j, lexema, &c);
+                break;
+            case 2:
+                // token = verificar se é identificador, palavra reservada e booleano
+                lexema[j-1] = '\0';
+                i--;
+                return token, lexema;
+            case 3:
+                if(isdigit(c)){;}
+                else if(isalpha(c)){erro();}
+                else if(c == '.'){estado = 5}
+                else{estado = 4;}
+                avanca(&j, lexema, &c);
+                break;
+            case 4:
+                lexema[j-1] = '/0';
+                token = "num_inteiro";
+                i--;
+                return token, lexema;
+            case 5:
+                if(isdigit(c)){estado = 6;}
+                else{erro();}
+                avanca(&j, lexema, &c);
+                break;
+            case 6:
+                if(isdigit(c)){;}
+                else if(isalpha(c)){erro();}
+                else{estado = 7;}
+                avanca(&j, lexema, &c);
+                break;
+            case 7:
+                lexema[j-1] = '\0';
+                token = "num_real";
+                i--;
+                return token, lexema;
+            case 8:
+                if(c == '\n'){erro();}
+                else if(c == '\"'){estado = 9;}
+                else{;}
+                avanca(&j, lexema, &c);
+                break;
+            case 9:
+                token = "string";
+                i--;
+                return token, lexema;
+            case 10:
+                if(c == '\n'){erro();}
+                else{estado = 11;}
+                avanca(&j, lexema, &c);
+                break;
+            case 11:
+            //verifica char ?
+                if(c == '\''){estado = 12;}
+                else{erro();}
+            case 12:
+                token = "char";
+                i--;
+                return lexema, token;
         }
-        
     }
 }
 
-int main(int agrc, char *argv[]){
+void concatenar_letra(char *str, char ch){
+    long int size = strlen(str);
+    str[size] = ch;
+    str[size + 1] = '\0'; //recolocar terminador
+}
 
-    FILE *teste = fopen(argv[1], "rb"); //abertura do arquivo
-    if(teste == NULL){
-        printf("ERRO!");
+void analexLine(char *str){
+    while(i < strlen(str)){
+        char token[200] = ""; //Verificar o tamanho do maior token possivel +1
+        char lexema[200] = "";
+        char ch;
+        while(token != ""){
+            ch = prox_char(str);
+            concatenar_letra(lexema,ch);
+            // strcpy(token,analex(lexema));
+        }
+        grava_token(token,lexema);
+    }
+    i = 0;
+}
+
+void main(int agrc, char *argv[]){
+
+    FILE *file = fopen(argv[1], "rb"); //abertura do arquivo
+    if(file == NULL){
+        printf("ERRO AO ABRIR O CODIGO!");
     }
 
-    char str[200];
-    fgets(str, 200, teste);
-    for(int j = 0; j <11; j++){printf("%c", prox_char(str));}
-    printf("\n");
+    docLex = fopen("docLex.txt","wb");
+    if(strcmp("a","a")){
+        printf("OK\n");
+    }
+    else{
+        printf("No\n");
+    }
+    //Enquanto o documento ainda não tiver acabado rodará o while
+    while(!feof(file)){
+        /*a = 0;
+        token = id*/
 
-    fgets(str, 200, teste);
-    for(int j = 0; j <11; j++){printf("%c", prox_char(str));}
-    printf("\n");
-    
-    fclose(teste);
-    
+        token, lexema = analex();
+        gravar_token(token, lexema);
+    }
+
+    fclose(file);
+    fclose(docLex);  
 }
